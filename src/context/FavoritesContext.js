cecd64fea900ee7ff1fdbb488b2d8369ecc61a61
Gabriel ML <gabrielmljob@gmail.com>
2025-07-15 23:27:00 +0200

    style: Add clarification on how to use the 'Context API'

--- src/context/FavoritesContext.js
+++ src/context/FavoritesContext.js
@@ -2,7 +2,11 @@
 import { createContext } from "react";
 
 /**
- * 1. We create the 'Context'.
+ * Part 1 of 3 of 'Context API' (createContext, Provider, useContext)
+ * This is a cornerstone of modern React applications for managing
+ * global state.
+ * 
+ * 1. We create the 'Context' (createContext)
  * 'createContext' creates a 'hole' in our 'Component Tree' that can
  * be filled with data.
  * This is what Components will 'consume' to get the 'favorites state'.
