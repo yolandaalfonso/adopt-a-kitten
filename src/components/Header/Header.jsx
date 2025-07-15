1c6c89f2d10ea201cde4971af17296afa9d838f9
Aymee <areinar31@gmail.com>
2025-07-15 14:29:22 +0200

    fix/Arreglo de links en Header de Inicio/Adopta/Noticias/Contacto

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -19,11 +19,11 @@ const Header = () => {
                 
                 <div className={`menu ${menuOpen ? 'open' : ''}`}>
                     <ul>
-                        <li><a href="#" onClick={() => setMenuOpen(false)}>INICIO</a></li>
-                        <li><a href="#" onClick={() => setMenuOpen(false)}>ADOPTA</a></li>
+                        <li><a href='/' onClick={() => setMenuOpen(false)}>INICIO</a></li>
+                        <li><a href='/adopt' onClick={() => setMenuOpen(false)}>ADOPTA</a></li>
                         <li><a href="#" onClick={() => setMenuOpen(false)}>COLABORA</a></li>
-                        <li><a href="#" onClick={() => setMenuOpen(false)}>NOTICIAS</a></li>
-                        <li><a href="#" onClick={() => setMenuOpen(false)}>CONTACTO</a></li>
+                        <li><a href="https://www.santevet.es/categorias/actualidad-gatos" target="_blank" rel="noopener noreferrer" onClick={() => setMenuOpen(false)}>NOTICIAS</a></li>
+                        <li><a href="#contacto" onClick={() => setMenuOpen(false)}>CONTACTO</a></li>
                     </ul>
                 </div>
             </div>
