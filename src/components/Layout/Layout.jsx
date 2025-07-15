6f259078d8586e484972ba3ae974128dcc83b0b4
Gabriel ML <gabrielmljob@gmail.com>
2025-07-16 00:46:28 +0200

    style: Formatting document, no code changes

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -3,16 +3,14 @@ import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import { Outlet } from "react-router-dom";
 
-const Layout=({ children })=>{
-return(
-<>
-<Header/>
- <main>
-    {children}
-</main>
-<Footer/>
-</>
-);
-}
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+};
 
 export default Layout;
