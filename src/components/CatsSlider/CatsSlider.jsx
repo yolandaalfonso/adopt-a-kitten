1d9f57c987d4422d18fa1c452be6d957edff8d16
Gabriel ML <gabrielmljob@gmail.com>
2025-07-16 14:59:45 +0200

    refactor: Rename variables for improving readability
    -[x] The state variable 'images' to 'cats'
    -[x] The local variable 'cats' to 'fetchedCats' (inside 'fetchData')
    
    This increase 'Clarity', 'Scope Management' and 'Consistency'

--- src/components/CatsSlider/CatsSlider.jsx
+++ src/components/CatsSlider/CatsSlider.jsx
@@ -1,21 +1,21 @@
 import React, { useEffect, useRef, useState } from "react";
 import { fetchCatsWithBreeds } from "../../services/catService";
 import CatCard from "../CatCard/CatCard";
-import "./CatsSlider.css"; // Asegúrate de tener un archivo CSS para estilos adicionales
+import "./CatsSlider.css";
 import { useFavorites } from "../../context/useFavorites"; // Import our custom hook to access the favorites context
 
 export const CatsSlider = () => {
   const carouselRef = useRef(null);
-  const [images, setImages] = useState([]);
+  const [cats, setCats] = useState([]);
 
   // Use our custom hook to get the 'favorites state' and 'dispatch function'
   const { favorites, dispatch } = useFavorites();
 
   useEffect(() => {
     const fetchData = async () => {
-      const cats = await fetchCatsWithBreeds(10);
-      console.log(cats); // TODO: TO_DEL - ONLY FOR TESTING!
-      setImages(cats); // ahora son objetos completos, no solo URLs
+      const fetchedCats = await fetchCatsWithBreeds(10);
+      console.log(fetchedCats); // TODO: TO_DEL - ONLY FOR TESTING!
+      setCats(fetchedCats);
     };
 
     fetchData();
@@ -60,7 +60,7 @@ export const CatsSlider = () => {
     <div>
       <div className="carousel-container">
         <div ref={carouselRef} className="carousel">
-          {images.map((cat, index) => {
+          {cats.map((cat, index) => {
             // Determine if the current cat is in the 'favorites list'
             const isFavorite = favorites.some((favCat) => favCat.id === cat.id);
 
