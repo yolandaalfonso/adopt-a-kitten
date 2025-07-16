73302549ea36afe4423812b9cff5097b4d38c734
Yolanda <yolandaalfonsolopez@gmail.com>
2025-07-16 14:51:06 +0200

    feat: Add dark mode to HomePage

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import './Header.css';
+import { ThemeContext } from '../../context/ThemeContext';
 
 const Header = () => {
     const [menuOpen, setMenuOpen] = useState(false);
+    const { theme, toggleTheme } = useContext(ThemeContext);
+
     
     const toggleMenu = () => setMenuOpen(!menuOpen);
 
@@ -10,7 +13,7 @@ const Header = () => {
         <nav className="header-nav">
             <div className="nav-container">
                 <a href="#" className="logo-link">
-                    <img className="logo" src="../../public/MiauMatch.png" alt="logo MiauMatch" />
+                    <img className="logo" src={theme === 'dark' ? '/MiauMatch-dark.png' : '/MiauMatch.png'} alt="logo MiauMatch" />
                 </a>
                 
                 <button className="button-toggle" onClick={toggleMenu}>
@@ -21,11 +24,16 @@ const Header = () => {
                     <ul>
                         <li><a href="#" onClick={() => setMenuOpen(false)}>INICIO</a></li>
                         <li><a href="#" onClick={() => setMenuOpen(false)}>ADOPTA</a></li>
-                        <li><a href="#" onClick={() => setMenuOpen(false)}>COLABORA</a></li>
+                        <li><a href="#" onClick={() => setMenuOpen(false)}>FAVORITOS</a></li>
                         <li><a href="#" onClick={() => setMenuOpen(false)}>NOTICIAS</a></li>
                         <li><a href="#" onClick={() => setMenuOpen(false)}>CONTACTO</a></li>
                     </ul>
                 </div>
+            </div>
+            <div className="theme-toggle-wrapper">
+                <button className="theme-toggle-btn" onClick={toggleTheme}>
+                    {theme === 'light' ? '🌙' : '☀️'}
+                </button>
             </div>
         </nav>
     );
