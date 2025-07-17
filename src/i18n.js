a1d83cdd8bf941e1e1bd4307b77a82ba4c1bb5e9
Aymee <areinar31@gmail.com>
2025-07-17 14:50:13 +0200

    fix/Arreglado navegacion entre paginas manteniendo idioma

--- src/i18n.js
+++ src/i18n.js
@@ -3,19 +3,25 @@ import { initReactI18next } from 'react-i18next'
 
 import en from './locales/en.json'
 import es from './locales/es.json'
+import LanguageDetector from 'i18next-browser-languagedetector'
 
 i18n
+    .use(LanguageDetector) // ← usa el detector
     .use(initReactI18next)
     .init({
         resources: {
-            en: { translation: en },
             es: { translation: es },
+            en: { translation: en },
         },
-        lng: 'es', // Idioma por defecto
         fallbackLng: 'es',
+        detection: {
+            // Aquí se define el orden y almacenamiento
+            order: ['localStorage', 'navigator'],
+            caches: ['localStorage'],
+        },
         interpolation: {
-            escapeValue: false
-        }
+            escapeValue: false,
+        },
     })
 
 export default i18n
